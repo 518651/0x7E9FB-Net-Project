0dd07e00b22c80338cf8c6e7a6e67003fc0f8b0c
518651 <69994181+518651@users.noreply.github.com>
2021-11-12 12:26:21 +0800

    Anti sendbox

---
+++ 0x7E9FB/Anti sendbox.cpp
@@ -0,0 +1,77 @@
+#include "Un-main.h"
+
+DWORD get_parent_processid(DWORD pid)
+{
+	DWORD ParentProcessID = -1;
+
+	PROCESSENTRY32 pe;
+
+	HANDLE hkz;
+
+	HMODULE hModule = LoadLibrary(_T("Kernel32.dll"));
+
+	FARPROC Address = GetProcAddress(hModule, "CreateToolhelp32Snapshot");
+
+	if (Address == NULL) {
+		OutputDebugString(_T("GetProc error"));
+		return(-1);
+	}
+
+	_asm {
+		push 0
+		push 2
+		call Address
+		mov hkz, eax
+	}
+
+	pe.dwSize = sizeof(PROCESSENTRY32);
+
+	if (Process32First(hkz, &pe)) {
+		do {
+			if (pe.th32ProcessID == pid) {
+				ParentProcessID = pe.th32ParentProcessID;
+				break;
+			}
+		} while (Process32Next(hkz, &pe));
+	}
+	return ParentProcessID;
+}
+
+
+DWORD get_explorer_processid() {
+	DWORD explorer_id = -1;
+	PROCESSENTRY32 pe;
+	HANDLE hkz;
+	HMODULE hModule = LoadLibrary(_T("Kernel32.dll"));
+
+	if (hModule == NULL) {
+		OutputDebugString(_T("Loaddll error"));
+		return(-1);
+	}
+	FARPROC Address = GetProcAddress(hModule, "CreateToolhelp32Snapshot");
+
+	if (Address == NULL) {
+		OutputDebugString(_T("GetProc error"));
+		return(-1);
+	}
+
+	_asm {
+		push 0
+		push 2
+		call Address
+		mov hkz, eax
+	}
+
+	pe.dwSize = sizeof(PROCESSENTRY32);
+
+	if (Process32First(hkz, &pe)) {
+		do {
+			if (_wcsicmp((const wchar_t*)pe.szExeFile, L"explorer.exe") == 0)
+			{
+				explorer_id = pe.th32ProcessID;
+				break;
+			}
+		} while (Process32Next(hkz, &pe));
+	}
+	return explorer_id;
+}
